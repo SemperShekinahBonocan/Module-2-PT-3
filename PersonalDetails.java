228217b37a2aef53a2af72d8e10d0eb4e9d539bc
SemperShekinahBonocan <ssBonocan@mcm.edu.ph>
2025-01-28 09:47:34 +0800

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,13 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Semper Shekinah C. Bonocan";
+        int age = 18;
+        double height = 1.45;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Registered Student? " + isStudent);
+    }
+}
